1af80822a6144179a5745a07fba4ad20955fd520
alirezazamirian <alirezazamirian@gmail.com>
2024-11-07 18:55:34 +0330

    edit cors 2

--- scientificlab/settings.py
+++ scientificlab/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'scientificlab.urls'
