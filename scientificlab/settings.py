3ba63a63447b1022d56e167c9e17cb534bba6d0a
alireza <alirezazamirian@gmail.com>
2025-02-27 15:09:23 +0330

    feat: add pagination globaly

--- scientificlab/settings.py
+++ scientificlab/settings.py
@@ -124,7 +124,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 # Internationalization
